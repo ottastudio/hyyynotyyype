c347138086b75fbace795c5de0f41e8d555b668e
ottta <oktamataufik@gmail.com>
2020-06-02 10:24:49 +0700

    Create custom document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        </Head>
+        <body>
+          <script src="/noflash.js" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
