79253da66cdbc2a18dfbbe05d1f44a0b21ae7f87
ottta <oktamataufik@gmail.com>
2020-06-04 00:28:41 +0700

    Bring back global style to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,6 +57,102 @@ export default class extends App<{ isMobile: boolean }> {
             )}
           </PageTransition>
         </AppProvider>
+
+        <style jsx global>{`
+          :root {
+            --background: #f5f5f5;
+            --foreground: #202020;
+            --tab-background: #f5f5f5;
+            --tab-foreground: #dedede;
+            --lightgrey: #dedede;
+            --highlight: #0000ff;
+            --deeppink: #ff1493;
+
+            --light-line: #efefef;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+
+          body {
+            margin: 0;
+            padding: 0;
+            font-family: "Helvetica Neue", sans-serif;
+            background-color: var(--background);
+            color: var(--foreground);
+            overscroll-behavior-y: none;
+          }
+
+          .theme--light {
+            --background: #f5f5f5;
+            --foreground: #202020;
+            --tab-background: #f5f5f5;
+            --tab-foreground: #dedede;
+            --lightgrey: #dedede;
+            --highlight: #0000ff;
+            --deeppink: #ff1493;
+
+            --light-line: #efefef;
+          }
+          .theme--dark {
+            --background: #202020;
+            --foreground: #e2e2e2;
+            --tab-background: #202020;
+            --tab-foreground: #0c0c0c;
+            --lightgrey: #0c0c0c;
+            --highlight: #adff2f;
+            --deeppink: #ff1493;
+
+            --light-line: #1d1d1d;
+          }
+
+          #__next {
+            background-color: inherit;
+            color: inherit;
+          }
+
+          a,
+          button,
+          input {
+            color: inherit;
+            text-decoration: none;
+            backface-visibility: hidden;
+            -webkit-tap-highlight-color: transparent;
+          }
+
+          .accordion-container-enter,
+          .phone-container-enter,
+          .accordion-container-enter-active,
+          .phone-container-enter-active {
+            opacity: 0;
+          }
+          .accordion-container-enter-done,
+          .phone-container-enter-done {
+            opacity: 1;
+            transition: opacity 300ms cubic-bezier(1, 0, 0.5, 1);
+          }
+          .accordion-container-exit,
+          .phone-container-exit {
+            opacity: 1;
+            transition: opacity 150ms ease;
+          }
+          .accordion-container-exit-active,
+          .phone-container-exit-active,
+          .accordion-container-exit-done,
+          .phone-container-exit-done {
+            opacity: 0;
+          }
+
+          .dummy {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 5vw;
+            font-weight: bold;
+          }
+        `}</style>
       </>
     );
   }
