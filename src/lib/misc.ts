7e4288263c430ac93e132cee5bb34c873422908f
ottta <oktamataufik@gmail.com>
2020-06-03 16:52:41 +0700

    Create misc file

---
+++ src/lib/misc.ts
@@ -0,0 +1,11 @@
+export const SECTION_INDEX: string[] = ["/"];
+export const SECTION_TYPEFACES: string[] = ["/typefaces", "/typefaces/[name]"];
+export const SECTION_ACCOUNT: string[] = ["/account", "/account/[section]"];
+export const SECTION_ETC: string[] = ["/etc", "/etc/[section]"];
+
+export const accordionLabel = {
+  LABEL_INDEX: "Index",
+  LABEL_TYPEFACES: "Typefaces",
+  LABEL_ACCOUNT: "Account",
+  LABEL_ETC: "Etc",
+};
