4d13270082a322b261d46b229130df21159a6b57
ottta <oktamataufik@gmail.com>
2020-06-04 13:10:18 +0700

    Back to margin

--- src/components/Phone/PhoneContainer.tsx
+++ src/components/Phone/PhoneContainer.tsx
@@ -18,11 +18,12 @@ export const PhoneContainer: React.FC<{}> = ({ children }) => {
         .phone-container {
           background-color: var(--background);
           position: relative;
-          padding: calc(60px + 0.5rem) 0.5rem 0.5rem 0.5rem;
-          transition: padding 300ms cubic-bezier(1, 0, 0, 1);
+          margin-top: 60px;
+          padding: 0.5rem;
+          transition: margin-top 300ms cubic-bezier(1, 0, 0, 1);
         }
         .phone-container-with-drawer {
-          padding: calc(50vh + 0.5rem) 0.5rem 0.5rem 0.5rem;
+          margin-top: 50vh;
         }
       `}</style>
     </>
